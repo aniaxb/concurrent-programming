845e91b92377b61cd6d1c20f5a34db811db56932
Ania Banachowicz <101459177+aniaxb@users.noreply.github.com>
2024-04-02 21:47:10 +0200

    Remodeled projects to be .net standard 2.0 and implemented initial Data and View along with ViewModel

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Data;
+
+namespace ViewModel
+{
+    public class MainViewModel : ObservableRecipient
+    {
+        public MainViewModel()
+        {
+            UpdateBallsCommand = new RelayCommand<int>(UpdateBalls);
+            StartMovingCommand = new RelayCommand(StartMoving, CanStartMoving);
+            StopMovingCommand = new RelayCommand(StopMoving, CanStopMoving);
+        }
+
+        public ObservableCollection<Ball> Balls { get; } = new ObservableCollection<Ball>();
+
+        public ICommand UpdateBallsCommand { get; }
+        public ICommand StartMovingCommand { get; }
+        public ICommand StopMovingCommand { get; }
+
+        private void UpdateBalls(int numberOfBalls)
+        {
+            Balls.Clear();
+            var random = new Random();
+            for (int i = 0; i < numberOfBalls; i++)
+            {
+                var ball = new Ball
+                {
+                    BallId = i,
+                    XPosition = random.NextDouble() * 300,
+                    YPosition = random.NextDouble() * 300,
+                    XDirection = random.NextDouble(),
+                    YDirection = random.NextDouble(),
+                    color = $"#{random.Next(0x1000000):X6}"
+                };
+                Balls.Add(ball);
+            }
+        }
+
+        private bool CanStartMoving() => true; // Warunki potrzebne do uruchomienia animacji
+        private void StartMoving()
+        {
+            // Rozpocznij animację
+        }
+
+        private bool CanStopMoving() => true; // Warunki potrzebne do zatrzymania animacji
+        private void StopMoving()
+        {
+            // Zatrzymaj animację
+        }
+    }
+}
