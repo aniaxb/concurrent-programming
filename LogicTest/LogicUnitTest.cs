29db45682e4f7728c25ede66037352daa8f2965a
Katarzyna Pietrzyk <katarzynapietrzyk2806@gmail.com>
2024-05-26 20:12:12 +0200

    wszystkie pliki etap 3

--- LogicTest/LogicUnitTest.cs
+++ LogicTest/LogicUnitTest.cs
@@ -2,6 +2,9 @@ using NUnit.Framework;
 using Logic;
 using Data;
 using System.Collections.Generic;
+using NUnit.Framework.Internal;
+using Moq;
+
 
 namespace LogicTest
 {
@@ -9,12 +12,14 @@ namespace LogicTest
     {
         private BallLogic ballLogic;
         private BallApi ball;
+        private Logger logger;
 
         [SetUp]
         public void Setup()
         {
             ball = BallApi.CreateBall(1, 150, 150, 1, 1, "#FF0000", 10, 20);
-            ballLogic = new BallLogic(ball);
+            logger = new Logger("test_log.json");
+            ballLogic = new BallLogic(ball, logger);
         }
 
         [Test]
@@ -45,7 +50,10 @@ namespace LogicTest
             List<BallLogic> balls = new List<BallLogic>();
 
             balls.Add(ballLogic);
-            balls.Add(new BallLogic(otherBall));
+            balls.Add(new BallLogic(otherBall, logger));
+
+            ballLogic.Move(balls);
+            balls.Add(new BallLogic(otherBall, logger));
 
             ballLogic.Move(balls);
 
@@ -66,6 +74,33 @@ namespace LogicTest
             Assert.That(initialXDirection, Is.Not.EqualTo(ball.XDirection), "XDirection of ball1 should change after collision");
             Assert.That(initialXDirection, Is.Not.EqualTo(ball.XDirection), "YDirection of ball1 should change after collision");
 
+        }
+        [Test]
+        public void Logger_ShouldLogData()
+        {
+            var mockLogger = new Mock<Logger>("mock_log.json");
+            var ball = BallApi.CreateBall(1, 100, 100, 1, 1, "#FF0000", 10, 20);
+            var ballLogic = new BallLogic(ball, mockLogger.Object);
+
+            ballLogic.Move(new List<BallLogic>(), 1);
+
+            mockLogger.Verify(logger => logger.Log(It.IsAny<BallApi>()), Times.Once);
+        }
+
+        [Test]
+        public void Logging_ShouldNotAffectBallMovement()
+        {
+            var logger = new Logger("test_log.json");
+            var ball = BallApi.CreateBall(1, 100, 100, 1, 1, "#FF0000", 10, 20);
+            var ballLogic = new BallLogic(ball, logger);
+
+            double initialX = ball.XPosition;
+            double initialY = ball.YPosition;
+
+            ballLogic.Move(new List<BallLogic>(), 1);
+
+            Assert.AreNotEqual(initialX, ball.XPosition);
+            Assert.AreNotEqual(initialY, ball.YPosition);
         }
     }
 }
